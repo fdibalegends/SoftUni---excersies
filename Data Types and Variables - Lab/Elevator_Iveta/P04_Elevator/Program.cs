1b1c95f7e79816ce6051dbbf89b8e146b780564f
iveta <peychinova.iveta@gmail.com>
2017-09-30 13:28:57 +0300

    This is Iveta's Elevator

---
+++ Data Types and Variables - Lab/Elevator_Iveta/P04_Elevator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P04_Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberPeople = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            int courses = numberPeople / capacity; //17 / 3 = 5
+            int leftPeople = numberPeople % capacity; //17 % 2 = 2
+            
+            if(leftPeople > 0)
+            {
+                Console.WriteLine(courses + 1);
+            }
+            else
+            {
+                Console.WriteLine(courses);
+            }
+        }
+    }
+}
