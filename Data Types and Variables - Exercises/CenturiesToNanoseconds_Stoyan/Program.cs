e93217bfbc2ea971fbd8489d1295d1155c177df3
kilodox <kilodox@abv.bg>
2017-10-02 13:27:55 +0300

    new Uploads

--- Data Types and Variables - Exercises/CenturiesToNanoseconds_Stoyan/Program.cs
+++ Data Types and Variables - Exercises/CenturiesToNanoseconds_Stoyan/Program.cs
@@ -10,6 +10,18 @@ namespace CenturiesToNanoseconds_Stoyan
     {
         static void Main(string[] args)
         {
+            int centuries = int.Parse(Console.ReadLine());
+            int years = centuries * 100;
+            int days = (int)(years * 365.2422);
+            long horus = days * 24;
+            long minutes = horus * 60;
+            long seconds = minutes * 60;
+            long miliseconds = seconds * 1000;
+            decimal microseconds = miliseconds * 1000M;
+            decimal nanoseconds = microseconds * 1000M;
+
+                Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {horus} hours = {minutes} minutes = {seconds} seconds = {miliseconds} milliseconds = {microseconds} microseconds = {nanoseconds} nanoseconds");
+                 
         }
     }
 }
---
+++ Data Types and Variables - Exercises/ConvertSpeedUnits_Stoyan/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertSpeedUnits_Stoyan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float distance = float.Parse(Console.ReadLine());
+            float hours = float.Parse(Console.ReadLine());
+            float minutes = float.Parse(Console.ReadLine());
+            float seconds = float.Parse(Console.ReadLine());
+
+            float sumSec = (hours * 3600) + (minutes * 60) + seconds;
+            float sumHours = sumSec / 3600;
+            float ms = distance / sumSec;
+            float kh = (distance / 1000) / sumHours;
+            float mh = (distance / 1609) / sumHours;
+            Console.WriteLine($"{ms.ToString():F6}");
+            Console.WriteLine($"{kh.ToString():F6}");
+            Console.WriteLine($"{mh.ToString():F6}");
+
+        }
+    }
+}
